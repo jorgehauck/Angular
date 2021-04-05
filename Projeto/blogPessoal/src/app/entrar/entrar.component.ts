d8072ac08ccc4314c73574395123689bf5e096d3
jorgehauck <jorge.hauck1997@outlook.com>
2021-04-05 13:38:15 -0300

    Implementação do usuario adm

--- Projeto/blogPessoal/src/app/entrar/entrar.component.ts
+++ Projeto/blogPessoal/src/app/entrar/entrar.component.ts
@@ -41,6 +41,8 @@ export class EntrarComponent implements OnInit {
       environment.nome = this.userLogin.nome
       environment.foto = this.userLogin.foto
       environment.id = this.userLogin.id
+      environment.tipo = this.userLogin.tipo
+
       
       this.userLogin.foto
 
